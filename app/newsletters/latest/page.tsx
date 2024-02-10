15d894637db9b6c6918099a026ed0dacd09b2bb9
Ray <ronaga@bridgesforpeace.com>
2024-02-10 09:17:11 +0200

    commit

---
+++ app/newsletters/latest/page.tsx
@@ -0,0 +1,5 @@
+const NewslettersLatest = () => {
+    location.href = "SHINE　ON !2024.1.pdf";
+    return <div className=" items-center justify-center text-6xl">コンテンツを鋭意製作中！</div>;
+};
+export default NewslettersLatest;
---
+++ app/newsletters/page.tsx
@@ -0,0 +1,4 @@
+const Newsletters = () => {
+    return <div className=" items-center justify-center text-6xl">コンテンツを鋭意製作中！</div>;
+};
+export default Newsletters;
