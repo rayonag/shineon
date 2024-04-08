ca79c7bd35f5e23cc91a9dec5caf848df34d77c2
Ray <ronaga@bridgesforpeace.com>
2024-04-08 07:28:10 +0300

    jigsaw to be continued

---
+++ app/game/jigsaw/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import React from 'react';
+import './script.js';
+import './style.css';
+
+const Jigsaw = () => {
+    return (
+        <>
+            <div>
+                <h1>Jigsaw</h1>
+            </div>
+            <div id="forPuzzle"></div>
+        </>
+    );
+};
+export default Jigsaw;
